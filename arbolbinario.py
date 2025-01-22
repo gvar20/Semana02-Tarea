f75b35d75d1298fc1d6ef2d9ee7952133ea8f6e7
gvar20 <gui.ampuero.razuri@gmail.com>
2025-01-21 21:16:31 -0500

    se agrego el metodo de eliminar para el arbol binario

--- arbolbinario.py
+++ arbolbinario.py
@@ -35,6 +35,32 @@ class ArbolBinario:
             print(nodo.valor)
             self._MostrarRecursivo(nodo.derecho)
 
+    def ELiminar(self, valor):
+        self.raiz = self._EliminarRecursivo(self.raiz, valor)
+        if self.raiz == None:
+            print("El arbol esta vacio")
+
+    def _EliminarRecursivo(self, nodo, valor):
+        if nodo is None:
+            return None
+        if valor < nodo.valor:
+            nodo.izquierdo = self._EliminarRecursivo(nodo.izquierdo, valor)
+        elif valor > nodo.valor:
+            nodo.derecho = self._EliminarRecursivo(nodo.derecho, valor)
+        else:
+            if nodo.izquierdo is None:
+                return nodo.derecho
+            elif nodo.derecho is None:
+                return nodo.izquierdo
+            nodo.valor = self._EncontrarMinimo(nodo.derecho)
+            nodo.derecho = self._EliminarRecursivo(nodo.derecho, nodo.valor)
+        return nodo
+
+    def _EncontrarMinimo(self, nodo):
+        actual = nodo
+        while actual.izquierdo is not None:
+            actual = actual.izquierdo
+        return actual.valor
 
 arbol = ArbolBinario()
 arbol.Insertar(15)
@@ -43,3 +69,13 @@ arbol.Insertar(5)
 arbol.Insertar(16)
 
 arbol.Mostrar()
+print("ELiminar 20 del arbol")
+arbol.ELiminar(20)
+arbol.Mostrar
+
+
+print("Eliminar mostrar arbol vacio")
+arbol.ELiminar(15)
+arbol.ELiminar(5)
+arbol.ELiminar(16)
+arbol.Mostrar

5e03ccc05fc1e2dc480f6618f05e8ff3f8b03c51
gvar20 <gui.ampuero.razuri@gmail.com>
2025-01-21 20:37:47 -0500

    se agrego el metodo Eliminar a la lista enlazada

--- listaenlazada.py
+++ listaenlazada.py
@@ -24,10 +24,28 @@ class ListaEnlazada:
             actual = actual.siguiente
 
 
+    def ELiminar(self, valor):
+        if self.cabeza is None:
+            return
+        if self.cabeza.valor == valor:
+            self.cabeza == self.cabeza.siguiente
+            return
+        actual = self.cabeza
+        while actual.siguiente is not None:
+            if actual.siguiente.valor == valor:
+                actual.siguiente = actual.siguiente.siguiente
+                return
+            actual = actual.siguiente
+
+
+
 lista = ListaEnlazada()
 
 lista.Insertar(5)
 lista.Insertar(3)
 lista.Insertar(8)
 
+lista.Mostrar()
+print("Eliminar 3")
+lista.ELiminar(3)
 lista.Mostrar()
